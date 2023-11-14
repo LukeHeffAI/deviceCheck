ba956817f6d34996b5e1c65bb72c9d2f8c9271ee
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2023-11-14 16:15:38 +1030

    Update logging and notifs

--- deviceCheck.py
+++ deviceCheck.py
@@ -1,5 +1,6 @@
 import json
 import os
+import datetime
 import platform
 import subprocess
 from plyer import notification
@@ -54,31 +55,77 @@ def get_ram_info():
         return f"{ram.total / (1024 ** 3):.2f} GB"
     except ImportError:
         return "psutil not installed"
+    
+# Function to log changes to a file
+def log_change(message):
+    log_file = "device_changes.log"
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = f"{timestamp}: {message}\n"
 
-# Function to check and update device info
+    # Create or append to the log file
+    with open(log_file, "a") as file:
+        file.write(log_entry)
+
+    # Limit log file to last 6 months
+    limit_log_file(log_file)
+
+def limit_log_file(log_file):
+    six_months_ago = datetime.datetime.now() - datetime.timedelta(days=182)
+    with open(log_file, "r") as file:
+        lines = file.readlines()
+
+    with open(log_file, "w") as file:
+        for line in lines:
+            if datetime.datetime.strptime(line.split(":")[0], "%Y-%m-%d %H:%M:%S") > six_months_ago:
+                file.write(line)
+
+def compare_and_notify(current_info, baseline_info):
+    changes_detected = False
+    for device in current_info.keys():
+        if current_info[device] != baseline_info[device]:
+            message = f"{device} has changed. Detected at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}."
+            notification.notify(
+                title="Device Change Detected",
+                message=message,
+                app_name="Device Monitor"
+            )
+            log_change(f"{device} changed from {baseline_info[device]} to {current_info[device]}")
+            changes_detected = True
+
+            # Ask user to confirm if the change is legitimate
+            response = input(f"Change detected in {device}. Is this a legitimate change? (y/n): ")
+            if response.lower() == 'y':
+                baseline_info[device] = current_info[device]
+
+    if not changes_detected:
+        log_change("No changes detected in devices.")
+
 def check_devices():
     baseline_file = "device_baseline.json"
     current_info = {
         "CPU": get_cpu_info(),
         "SSD": get_ssd_info(),
         "GPU": get_gpu_info(),
-        "RAM": get_ram_info()
+        "RAM": get_ram_info(),
     }
 
+    # Check if the baseline file exists
     if not os.path.exists(baseline_file):
+        # If not, create a new baseline file with current info
         with open(baseline_file, "w") as file:
-            json.dump(current_info, file)
-        return
+            json.dump(current_info, file, indent=4, sort_keys=True)
+        print("Baseline file created. No comparisons to make on first run.")
+        return  # Exit the function as there's nothing to compare yet
 
+    # If the baseline file exists, load it and compare
     with open(baseline_file, "r") as file:
         baseline_info = json.load(file)
 
-    if current_info != baseline_info:
-        notification.notify(
-            title="Device Change Detected",
-            message="One or more of your devices (CPU, SSD, GPU, RAM) have changed.",
-            app_name="Device Monitor"
-        )
+    compare_and_notify(current_info, baseline_info)
+
+    # Update baseline with current info after user confirmation
+    with open(baseline_file, "w") as file:
+        json.dump(baseline_info, file, indent=4, sort_keys=True)
 
 # Run the check
 check_devices()
