823a279da3ec8fb9b4a41c33154aa10dca3ddb1c
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2023-11-17 13:49:55 +1030

    Adding user checks

--- deviceCheck.py
+++ deviceCheck.py
@@ -11,6 +11,21 @@ import GPUtil
 import smtplib
 from email.message import EmailMessage
 
+def check_user_details():
+    if not os.path.exists("user_details.json"):
+        print("User details not found. Please enter your details.")
+        setup_user_details()  # Call the function to set up user details
+
+def setup_user_details():
+    user_details = {}
+    user_details['full_name'] = input("Enter your full name: ")
+    user_details['email'] = input("Enter your email address: ")
+
+    with open("user_details.json", "w") as file:
+        json.dump(user_details, file, indent=4)
+    
+    print("User details saved successfully.")
+
 # Function to get CPU info
 def get_cpu_info():
     cpu_info = {
@@ -95,6 +110,12 @@ def limit_log_file(log_file):
                 print(f"Error parsing date from log line: {line}. Error: {e}")
 
 def compare_and_notify(current_info, baseline_info):
+    try:
+        with open("user_details.json", "r") as file:
+            user_details = json.load(file)
+    except FileNotFoundError:
+        user_details = {"full_name": "Unknown User", "email": "no-reply@example.com"}
+
     changes_detected = False
     for device in current_info.keys():
         if current_info[device] != baseline_info[device]:
@@ -113,9 +134,11 @@ def compare_and_notify(current_info, baseline_info):
 
     # Ensure email is only sent once for a device change, and then reset once conflict resolved
     if changes_detected and not os.path.exists("email_sent.flag"):
-        send_email("Device Change Alert", "Changes detected in device configuration.", ["admin@example.com", "user@example.com"])
+        send_email("Device Change Alert for " + user_details.get("full_name", "Unknown User"), 
+                   "Changes detected in device configuration.", 
+                   ["admin@example.com", "user@example.com"], 
+                   user_details)
         open("email_sent.flag", "w").close()  # Create a flag file to indicate email has been sent
-
     elif not changes_detected and os.path.exists("email_sent.flag"):
         os.remove("email_sent.flag")  # Remove the flag file if no changes are detected
 
@@ -123,12 +146,16 @@ def compare_and_notify(current_info, baseline_info):
 def send_email(subject, body, recipient_emails):
     sender_email = "your_email@example.com"
     sender_password = "your_password"
+    user_full_name = user_details.get("full_name", "Unknown User")
+    user_email = user_details.get("email", "no-reply@example.com")
+
+    body = f"User: {user_full_name}\nEmail: {user_email}\n\n{body}"
 
     msg = EmailMessage()
     msg.set_content(body)
     msg['Subject'] = subject
     msg['From'] = sender_email
-    msg['To'] = ", ".join(recipient_emails)
+    msg['To'] = recipient_emails + [user_email]  # Add user's email to the recipient list
 
     try:
         with smtplib.SMTP_SSL('smtp.example.com', 465) as smtp:  # Replace with your SMTP server details
